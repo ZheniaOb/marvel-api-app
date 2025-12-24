093ada9e1b7126ff9e73036e643d0366aa1a03a3
ZheniaOb <spinand3@gmail.com>
2025-12-24 19:10:51 +0100

    Refactor RandomChar component and MarvelService methods for improved character fetching and rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then((res) => {
-    console.log(res);
-});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
