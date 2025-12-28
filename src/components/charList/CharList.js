ab1465ac4113bc1c6f156209623dd49ddaff445e
ZheniaOb <spinand3@gmail.com>
2025-12-28 03:05:43 +0100

    Refactor CharList and MarvelService for improved character loading and error handling; add Spinner and ErrorMessage components

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,57 +1,105 @@
-import { Component } from 'react';
+import {Component} from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 class CharList extends Component {
+
     state = {
-        charList: []
+        charList: [],
+        loading: true,
+        error: false,
+        newItemLoading: false,
+        offset: 0,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.updateCharList();
-    }
-
-
-    updateCharList = () => {
-        this.marvelService
-        .getAllCharacters()
-        .then(this.onCharListLoaded)
-        .catch(this.onError);
+        this.onRequest();
     }
     
-    onCharListLoaded = (charList) => {
-        const shuffled = [...charList].sort(() => Math.random() - 0.5);
-        const randomNine = shuffled.slice(0, 9);
+    onRequest = (offset) => {
+        this.onCharListLoading();
+       this.marvelService.getAllCharacters(offset)
+            .then(this.onCharListLoaded)
+            .catch(this.onError)
+    }
 
+    onCharListLoading = () => {
         this.setState({
-            charList: randomNine
+            newItemLoading: true
         })
     }
-    render() {
-        const {charList} = this.state;
-        const items = charList.map((item) => {
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+
+        this.setState(({charList, offset}) => ({
+            charList: [...charList, ...newCharList],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+
+    renderItems(arr) {
+        const items =  arr.map((item) => {
             return (
-                <li className="char__item"
-                 key={item.id}
-                 onClick = {() => this.props.onCharSelected(item.id)}>
-                    <img src={item.thumbnail} alt={item.name}/>
-                    <div className="char__name">{item.name}</div>
+                <li 
+                    className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                        <img src={item.thumbnail} alt={item.name}/>
+                        <div className="char__name">{item.name}</div>
                 </li>
             )
         });
+ 
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
+
+    render() {
+
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
 
         return (
             <div className="char__list">
-                <ul className="char__grid">
-              
-                    {items}
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
+                {errorMessage}
+                {spinner}
+                {content}
+                <button 
+                className="button button__main button__long"
+                disabled={newItemLoading}
+                style={{'display': charEnded ? 'none' : 'block'}}
+                onClick={() => this.onRequest(offset)}>
+                    <div className="inner" >load more</div>
+                </button>
+            </div>
         )
     }
 }
